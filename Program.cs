556c30bb19aa631f59d400e82ac6a8f91465569f
poojakashyap-2920 <pooja337026@gmail.com>
2024-03-05 21:47:14 +0530

    Employee Wage Project added

---
+++ Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+public class EmployeeWageCalculator
+{
+    public static void Main(string[] args)
+    {
+        while (true)
+        {
+
+            Console.WriteLine("Enter the employee worked hours:");
+            int hr = Convert.ToInt32(Console.ReadLine());
+            int wage = 20; // Assuming wage is $20 per hour
+
+            switch (hr)
+            {
+                case 1:
+                case 2:
+                case 3:
+                case 4:
+                case 5:
+                case 6:
+                case 7:
+                case 8:
+                    Console.WriteLine($"Employee has worked {hr} hours per day & daily wage is {wage * hr}");
+                    break;
+                default:
+                    Console.WriteLine("Invalid number of hours!");
+                    break;
+            }
+
+        }
+        Console.ReadLine();
+    }
+}
